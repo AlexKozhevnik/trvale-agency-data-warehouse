85ead830c13773d6d92c9b5b5c32836ca2cb1b67
AlexKozhevnik <alex.kozhevnikovda@gmail.com>
2025-11-02 12:24:01 +0100

    Create init_database.sql for ItoDataWarehouse setup
    This script creates a new database named 'ItoDataWarehouse' and sets up three schemas: bronze, silver, and gold. It drops the database if it already exists before recreating it.

---
+++ scripts/init_database.sql
@@ -0,0 +1,34 @@
+/*
+Create Database and Schemas
+
+	This scrip creates a new database named 'ItoDataWarehouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. The script sets up three schemas: bronze, silver and gold
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'ItoDataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'ItoDataWarehouse')
+BEGIN
+	ALTER DATABASE ItoDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE ItoDataWarehouse;
+END;
+GO
+
+-- Create the ItoDataWarehouse database
+CREATE DATABASE ItoDataWarehouse;
+GO
+
+USE ItoDataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronz;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
