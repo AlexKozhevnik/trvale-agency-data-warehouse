fc2c38c3d9b9116d284592e8d66637d14e9ac9cc
AlexKozhevnik <alex.kozhevnikovda@gmail.com>
2025-11-15 17:29:19 +0100

    Enhance bronze.load_bronze procedure with logging
    Updated the stored procedure to log the number of rows loaded and the duration of the load process. Adjusted data types for timing variables and changed file paths for bulk insert and error logging.

--- scripts/bronze/store_procedure_load_bronze.sql
+++ scripts/bronze/store_procedure_load_bronze.sql
@@ -1,4 +1,4 @@
-/*
+ /*
 Store Procedure: Load Data from file to Bronze Layer (Source -> Bronze)
 
 Script Puprose: 
@@ -6,13 +6,17 @@ Script Puprose:
 	It performs the following actions:
 	- Truncate the bronze table before loading data.
 	- Uses the BULK INSERT command to load data from csv to bronze layer.
+	- Load log into special logs table
 
 How to execute: EXEC bronze.load_bronze
 */
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME;
+	DECLARE @start_time DATETIME2, @end_time DATETIME2;
+	DECLARE @rows_loaded INT;
+	DECLARE @duration_sec INT;
+
 	BEGIN TRY
 		PRINT '=========================================';
 		PRINT 'Loading Bronze Layer from ITO file';
@@ -24,19 +28,28 @@ BEGIN
 	
 		PRINT '>> Inserting Data Into: bronze.ito';
 		BULK INSERT bronze.ito
-		FROM 'C:\YOUR_FILE_PATH'
+		FROM 'C:\'
 		WITH (
 			FORMAT = 'CSV',
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ';',
 			ROWTERMINATOR = '0x0D0A',
 			CODEPAGE = '65001',
-			ERRORFILE = 'C:\YOUR_FILE_PATH',
+			ERRORFILE = 'C:\',
 			TABLOCK
 		);
+		SET @rows_loaded = @@ROWCOUNT;
 		SET @end_time = GETDATE();
-		PRINT '-- Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '-- SUCCESS: Data loaded into bronze.ito';
+		SET @duration_sec = DATEDIFF(SECOND, @start_time, @end_time);
+		
+		PRINT '-- Load Duration: ' + CAST(@duration_sec AS NVARCHAR) + ' seconds';
+		PRINT '-- SUCCESS ' + CAST(@rows_loaded AS NVARCHAR) + ' rows loaded into bronze.ito';
+
+		PRINT '=== INSERT LOAD LOG INTO bronze.ito_load_logs'
+		INSERT INTO bronze.ito_load_logs (source, number_of_rows, duration_seconds, status)
+		VALUES('ito_file', @rows_loaded, @duration_sec, 'SUCCESS');
+		PRINT '=== LOG ENTRY CREATED';
+
 	END TRY
 	BEGIN CATCH
 		PRINT '--------------------------------------------------'
@@ -44,5 +57,8 @@ BEGIN
 		PRINT '--------------------------------------------------'
 		PRINT 'Error message' + ERROR_MESSAGE();
 		PRINT 'Error message' + CAST(ERROR_NUMBER() AS NVARCHAR);
+
+		INSERT INTO bronze.ito_load_logs (source, number_of_rows, duration_seconds, status)
+		VALUES('ito_file', 0, DATEDIFF(SECOND, @start_time, GETDATE()), 'FAILED');
 	END CATCH
 END
